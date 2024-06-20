b820ba570c489a11a81365b2801da3652997f7e0
Caleb Ritchie <ritchie.caleb42@gmail.com>
2024-06-20 07:26:52 -0500

    test getAllProducts error path, data types for params

--- backend-lambda/services/productService.spec.js
+++ backend-lambda/services/productService.spec.js
@@ -15,55 +15,57 @@ describe("Product Service", () => {
     jest.clearAllMocks();
   });
 
-  it("should select all products", async () => {
-    // Setup
-    querySpy.mockResolvedValueOnce({ rows: [{ test: "test" }] });
-    const limit = 2;
-    const offset = 1;
+  describe("Get All Products", () => {
+    it("should select all products", async () => {
+      // Setup
+      querySpy.mockResolvedValueOnce({ rows: [{ test: "test" }] });
+      const limit = 2;
+      const offset = 1;
 
-    // Run
-    const res = await productService.getAllProducts(mockPool, limit, offset);
+      // Run
+      const res = await productService.getAllProducts(mockPool, limit, offset);
 
-    // Assert
-    expect(querySpy).toHaveBeenCalledWith(
-      "SELECT * FROM product LIMIT $1 OFFSET $2",
-      [limit, offset]
-    );
-    expect(res).toEqual([{ test: "test" }]);
-    expect(releaseSpy).toHaveBeenCalled();
-  });
+      // Assert
+      expect(querySpy).toHaveBeenCalledWith(
+        "SELECT * FROM product LIMIT $1 OFFSET $2",
+        [limit, offset]
+      );
+      expect(res).toEqual([{ test: "test" }]);
+      expect(releaseSpy).toHaveBeenCalled();
+    });
 
-  it("should throw an error and release client", async () => {
-    // Setup
-    querySpy.mockRejectedValueOnce(new Error("Error executing query:"));
-    const limit = 1;
-    const offset = 2;
+    it("should throw an error and release client", async () => {
+      // Setup
+      querySpy.mockRejectedValueOnce(new Error("Error executing query:"));
+      const limit = 1;
+      const offset = 2;
 
-    // Run & Assert
-    await expect(
-      productService.getAllProducts(mockPool, limit, offset)
-    ).rejects.toThrow("Error executing query:");
-    expect(releaseSpy).toHaveBeenCalled();
-  });
+      // Run & Assert
+      await expect(
+        productService.getAllProducts(mockPool, limit, offset)
+      ).rejects.toThrow("Error executing query:");
+      expect(releaseSpy).toHaveBeenCalled();
+    });
 
-  it("should throw an error if params are incorrect data type", async () => {
-    // Setup
-    const limit = 1;
-    const offset = 2;
+    it("should throw an error if params are incorrect data type", async () => {
+      // Setup
+      const limit = 1;
+      const offset = 2;
 
-    const poolMock = 2;
-    const limitMock = "limitMock";
-    const offsetMock = "offsetMock";
+      const poolMock = 3;
+      const limitMock = "limitMock";
+      const offsetMock = "offsetMock";
 
-    // Run & Assert
-    await expect(
-      productService.getAllProducts(poolMock, limit, offset)
-    ).rejects.toThrow("Pool must be an object");
-    await expect(
-      productService.getAllProducts(mockPool, limitMock, offset)
-    ).rejects.toThrow("Limit and offset must be numbers");
-    await expect(
-      productService.getAllProducts(mockPool, limit, offsetMock)
-    ).rejects.toThrow("Limit and offset must be numbers");
+      // Run & Assert
+      await expect(
+        productService.getAllProducts(poolMock, limit, offset)
+      ).rejects.toThrow("Pool must be an object");
+      await expect(
+        productService.getAllProducts(mockPool, limitMock, offset)
+      ).rejects.toThrow("Limit and offset must be numbers");
+      await expect(
+        productService.getAllProducts(mockPool, limit, offsetMock)
+      ).rejects.toThrow("Limit and offset must be numbers");
+    });
   });
 });
