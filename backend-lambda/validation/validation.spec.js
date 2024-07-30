976b337183ef748d9816769383a03fcb667c4fc2
Caleb <ritchie.caleb42@gmail.com>
2024-07-30 15:08:58 -0500

    validation extra test added

--- backend-lambda/validation/validation.spec.js
+++ backend-lambda/validation/validation.spec.js
@@ -1,17 +1,51 @@
 import validation from "./validation.js";
 
-describe("Validate Product Id", () => {
-  it("should return if product_id is in path", async () => {
-    // Setup
-    const id = 1;
-    // Run & Expect
-    expect(validation.validateProductId(id)).toEqual(1);
+describe("Validation Service", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  it("should throw an error if productId is not in path", async () => {
-    // Setup & Run & Expect
-    expect(() => validation.validateProductId()).toThrow(
-      "No product ID in path"
-    );
+  describe("Validate Product Id", () => {
+    it("should return if product_id is in path", async () => {
+      // Setup
+      const id = 1;
+      // Run & Expect
+      expect(validation.validateProductId(id)).toEqual(1);
+    });
+  
+    it("should throw an error if productId is not in path", async () => {
+      // Setup & Run & Expect
+      expect(() => validation.validateProductId()).toThrow(
+        "No product ID in path"
+      );
+    });
+  
+    it("should throw an error if productId is not a number", async () => {
+      expect(() => validation.validateProductId("eleven")).toThrow(
+        "Product ID is not a number"
+      )
+    });
+  });
+  
+  describe("Validate Category Id", () => {
+    it("should return if category_id is in path", async () => {
+      // Setup
+      const id = 1;
+      // Run & Expect
+      expect(validation.validateCategoryId(id)).toEqual(1);
+    });
+  
+    it("should throw an error if categoryId is not in path", async () => {
+      // Setup & Run & Expect
+      expect(() => validation.validateCategoryId()).toThrow(
+        "No category ID in path"
+      );
+    });
+  
+    it("should throw an error if categoryId is not a number", async () => {
+      expect(() => validation.validateCategoryId("eleven")).toThrow(
+        "Category ID is not a number"
+      )
+    });
   });
 });
