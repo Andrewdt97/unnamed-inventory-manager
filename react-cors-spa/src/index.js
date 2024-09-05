3c77b2ccd4d56480711f22c5823a914c79056c5e
ritchieritch42 <ritchie.caleb42@gmail.com>
2024-09-05 15:28:18 -0500

    initial react and react query setup

--- react-cors-spa/src/index.js
+++ react-cors-spa/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ReactQueryDevTools } from "@tanstack/react-query-devtools";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
