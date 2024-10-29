70c774e04603f33242c703817fa4acb7b9c0d44b
Caleb <ritchie.caleb42@gmail.com>
2024-10-28 19:49:24 -0500

    headers/rows/grid defined for demo

--- react-cors-spa/src/Products.js
+++ react-cors-spa/src/Products.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
-import "./Products.css";
+import { DataGrid } from '@mui/x-data-grid';
+import * as React from 'react';
 
 function fetchProducts() {
     const result = axios.get('http://127.0.0.1:3000/products?limit=5&offset=0');
@@ -17,15 +18,24 @@ function Products() {
         return <span>Loading...</span>
       }
     
-      if (status === 'error') {
+    if (status === 'error') {
         return <span>Error: {error.message}</span>
-      }
+    }
+
+    const rows = [
+        {id: JSON.stringify(data.data[0].product_id), col1: JSON.stringify(data.data[0].name)},
+        {id: JSON.stringify(data.data[1].product_id), col1: JSON.stringify(data.data[1].name)}
+    ];
+
+    const columns = [
+        { field: 'col1', headerName: 'Item', width: 150}
+    ];
 
     return (
-        <div>
-            <p>{JSON.stringify(data)}</p>
+        <div style={{ height: 300, width: '100%' }}>
+          <DataGrid rows={rows} columns={columns} />
         </div>
-    )
+      );
 }
 
 export default Products;
