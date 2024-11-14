2a65e6e54adefce2b9d39b904ae28891af0336c7
Caleb <ritchie.caleb42@gmail.com>
2024-11-14 14:56:57 -0600

    dialogue box

--- react-cors-spa/src/AddProduct.jsx
+++ react-cors-spa/src/AddProduct.jsx
@@ -1,5 +1,11 @@
+import * as React from 'react';
 import { Button } from "@mui/material";
 import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const AddProductButton = styled(Button)({
     color: 'white',
@@ -11,16 +17,44 @@ const AddProductButton = styled(Button)({
   });
 
 function AddProduct() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '10px' }}>
-            <AddProductButton
-            variant="outlined" 
-            onClick={() => {
-                alert('clicked');
-            }}
-            size="large"
-            >Add Product</AddProductButton>
-        </div>
+        <React.Fragment>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '10px' }}>
+                <AddProductButton variant="outlined" onClick={handleClickOpen} size="large">Add Product</AddProductButton>
+            </div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+            <DialogTitle id="alert-dialog-title">
+            {"Use Google's location service?"}
+            </DialogTitle>
+            <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                Let Google help apps determine location. This means sending anonymous
+                location data to Google, even when no apps are running.
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose}>Disagree</Button>
+            <Button onClick={handleClose} autoFocus>
+                Agree
+            </Button>
+            </DialogActions>
+            </Dialog>
+        </React.Fragment>
     )
 }
 
