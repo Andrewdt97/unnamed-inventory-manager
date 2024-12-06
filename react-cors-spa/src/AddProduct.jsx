493ea8efa0fb7d48a19752f687eecdd1a19bf1e5
Caleb <ritchie.caleb42@gmail.com>
2024-12-05 22:27:13 -0600

    refactored dialog box

--- react-cors-spa/src/AddProduct.jsx
+++ react-cors-spa/src/AddProduct.jsx
@@ -5,12 +5,20 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 import AddProductButton from './AddProductButton';
 import "./AddProductButton.css";
 import "./ProductDialogue.css";
 
 function AddProduct() {
     const [open, setOpen] = React.useState(false);
+    const categories = [
+        'Shirts',
+        'Pants',
+        'Shoes',
+        'Hat',
+        'Jewelry'
+    ];
 
     const handleClickOpen = () => {
       setOpen(true);
@@ -20,34 +28,29 @@ function AddProduct() {
       setOpen(false);
     };
 
+
+    // Caleb TODO:
+    // Add list to pick the category from, checkout MUI's options that are not an ugly radio button
     return (
         <React.Fragment>
             <div class="AddProductButtonContainer">
                 <AddProductButton variant="outlined" onClick={handleClickOpen} size="large">Add Product</AddProductButton>
             </div>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-            <DialogTitle id="alert-dialog-title">
-            {"Add a New Product"}
-            </DialogTitle>
-            <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-                <form class='productdialogue'>
-                    <input name="name" placeholder='Name of Product' />
-                    <input name="description" placeholder='Product Description' />
-                </form>
-            </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose} autoFocus>
-                Submit
-            </Button>
-            </DialogActions>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add Product</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Enter the details of the new product below.
+                        </DialogContentText>
+                        <TextField placeholder='Name' autoFocus required margin="dense" fullWidth variant="standard"/>
+                        <TextField placeholder='SKU' autoFocus required margin="dense" fullWidth variant="standard"/>
+                        <TextField placeholder='Size' autoFocus required margin="dense" fullWidth variant="standard"/>
+                        <TextField placeholder='Description' autoFocus required margin="dense" fullWidth variant="standard"/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button type="Submit">Submit</Button>
+                    </DialogActions>
             </Dialog>
         </React.Fragment>
     )
