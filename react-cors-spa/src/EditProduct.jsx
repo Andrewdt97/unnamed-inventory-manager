cd3bfef54bc12149b3ac8ea95d62d4105016d88a
Caleb <ritchie.caleb42@gmail.com>
2025-04-01 21:48:16 -0500

    icon replaces X

--- react-cors-spa/src/EditProduct.jsx
+++ react-cors-spa/src/EditProduct.jsx
@@ -1,7 +1,16 @@
 import "./EditProduct.css";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 function EditProduct() {
-  return <div className="edit-icon">X</div>;
+  return (
+    <div className="edit-icon">
+      <EditOutlinedIcon
+        sx={{
+          fontSize: "medium",
+        }}
+      />
+    </div>
+  );
 }
 
 export default EditProduct;
